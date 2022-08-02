ce9652148443c64b74147bb89012a9ba3bbc737c
qzart45hi <qwert45hi@yandex.ru>
2022-08-02 16:18:56 +0300

    feat: initial locations database

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .locations_db import Region, Municipality, Settlement
---
+++ locations_db.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+from typing import Type, TypeVar
+
+from sqlalchemy import Column, ForeignKey, select
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.sqltypes import Integer, String, Text, Float
+
+from common import PydanticModel, Base
+
+t = TypeVar("t", bound="LocalBase")
+
+
+class LocalBase(Base):
+    __abstract__ = True
+
+    id = Column(Integer, primary_key=True)
+    name = Column(Text, nullable=False)
+    aliases = Column(Text, nullable=True)
+
+    BaseModel = PydanticModel.column_model(id=id, name=name, aliases=aliases)
+
+    @classmethod
+    def find_or_create(cls: Type[t], session, name: str, **kwargs) -> t:
+        entry = cls.find_by_name(session, name, **kwargs)
+        if entry is None:
+            entry = cls.create(session, name=name, **kwargs)
+        return entry
+
+
+class Region(LocalBase):
+    __tablename__ = "nq_regions"
+
+
+class Municipality(LocalBase):
+    __tablename__ = "nq_municipalities"
+
+    reg_id = Column(Integer, ForeignKey("nq_regions.id"), nullable=False)
+    reg = relationship("Region")
+
+    TempModel = LocalBase.BaseModel.nest_model(LocalBase.BaseModel, "region", "reg")
+
+
+class SettlementType(LocalBase):
+    __tablename__ = "nq_settlement_types"
+
+
+class Settlement(LocalBase):
+    __tablename__ = "nq_settlements"
+
+    mun_id = Column(Integer, ForeignKey("nq_municipalities.id"), nullable=False)
+    mun = relationship("Municipality")
+    type_id = Column(Integer, ForeignKey("nq_settlement_types.id"), nullable=False)
+    type = relationship("SettlementType")
+
+    population = Column(Integer, nullable=False)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
+    oktmo = Column(String(11), nullable=False)
+
+    FullModel = LocalBase.BaseModel.column_model(population, latitude, longitude, oktmo)
