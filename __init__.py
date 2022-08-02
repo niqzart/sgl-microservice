035315af9e6c8e79fd1d2cd2b05d7b4fcb422f97
qzart45hi <qwert45hi@yandex.ru>
2022-08-02 16:24:59 +0300

    feat: start on mub part (post)

--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
 from .locations_db import Region, Municipality, Settlement
+from .locations_mub import controller as mub_locations_controller
---
+++ locations_mub.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from flask_restx import Resource
+from flask_restx.reqparse import RequestParser
+from werkzeug.datastructures import FileStorage
+
+from common import sessionmaker
+from moderation import MUBNamespace, permission_index
+from .locations_db import Region, Municipality, SettlementType, Settlement
+
+manage_cities = permission_index.add_permission("manage locations")
+controller = MUBNamespace("locations", path="/locations/", sessionmaker=sessionmaker)
+
+
+@controller.route("/")
+class CitiesControlResource(Resource):
+    parser = RequestParser()
+    parser.add_argument("csv", location="files", type=FileStorage, required=True)
+
+    @permission_index.require_permission(controller, manage_cities, use_moderator=False)
+    @controller.argument_parser(parser)
+    def post(self, session, csv: FileStorage):
+        lines = csv.stream.readlines()
+        for line in lines[1:]:
+            line = line.decode("utf-8")
+            params = [term.strip() for term in line.strip().split(",")[1:]]
+            if len(params) != 9:
+                controller.abort(400, "Invalid line: " + line)
+            reg_name, mun_name, set_name, set_type, population, children, latitude, longitude, oktmo = params
+            if mun_name == "null":
+                mun_name = reg_name
+
+            reg = Region.find_or_create(session, reg_name)
+            mun = Municipality.find_or_create(session, mun_name, reg_id=reg.id)
+            settlement_type = SettlementType.find_or_create(session, set_type)
+
+            population = int(population) + int(children)
+            Settlement.create(session, mun_id=mun.id, type_id=settlement_type.id, name=set_name,
+                              oktmo=oktmo, population=population, latitude=float(latitude), longitude=float(longitude))
