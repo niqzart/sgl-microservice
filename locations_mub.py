6599723d8b74d27a09d172dd7963f4b0b348e739
qzart45hi <qwert45hi@yandex.ru>
2022-08-02 21:14:51 +0300

    feat: comparing strategies in mub-get

--- locations_mub.py
+++ locations_mub.py
@@ -14,31 +14,32 @@ from .locations_db import Place
 controller = MUBNamespace("locations", path="/locations/", sessionmaker=sessionmaker)
 
 CSV_HEADER = "id,region,municipality,settlement,type,population,children,latitude_dd,longitude_dd,oktmo"
-STRATEGIES = (0, 1, 2, 3, 4, 5)
 
 
 @controller.route("/")
 class CitiesControlResource(Resource):
     parser = RequestParser()
     parser.add_argument("search", required=True)
+    parser.add_argument("strategy", type=int, required=False)
 
     @controller.doc_abort(400, "Empty search")
     @permission_index.require_permission(controller, manage_locations, use_moderator=False)
     @controller.argument_parser(parser)
     @controller.marshal_list_with(Place.TempModel)
-    def get(self, session, search: str) -> list[Place]:
+    def get(self, session, search: str, strategy: int) -> list[Place]:
         if len(search) == 0:
             controller.abort(400, "Empty search")
-        times = {}
-        for i in range(6):
-            t = time()
-            result = Place.get_all(session, search, strategy=i)
-            times[i] = (time() - t) / 2
-        for i in range(5, -1, -1):
-            t = time()
-            result = Place.get_all(session, search, strategy=i)
-            times[i] += (time() - t) / 2
-        print(times)
+        t = time()
+        result = Place.get_all(session, search, strategy=strategy)
+        print(time() - t)
+
+        a = [(r.id, r.name) for r in result]
+        b = [(r.id, r.name) for r in Place.get_all(session, search, strategy=0 if strategy == 4 else 4)]
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                print(i, a[i], b[i])
+        print()
+
         return result
 
     parser = RequestParser()
