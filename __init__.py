7204d4b6cecaab433d7c5117310c07686393cf27
qzart45hi <qwert45hi@yandex.ru>
2022-08-14 04:26:16 +0300

    fix: convert mub controller to a setup function

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from .locations_cli import locations_cli_blueprint
 from .locations_db import Region, Municipality, Settlement, Place
 from .locations_ini import init_locations
+from .locations_mub import setup as mub_locations_setup
 from .locations_rst import setup as locations_setup
