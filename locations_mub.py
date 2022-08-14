f685399b27d5348d7672269b7cbefb4ed6962033
qzart45hi <qwert45hi@yandex.ru>
2022-08-14 04:26:12 +0300

    fix: convert mub controller to a setup function

--- locations_mub.py
+++ locations_mub.py
@@ -7,59 +7,63 @@ from flask_restx.reqparse import RequestParser
 from werkzeug.datastructures import FileStorage
 
 from common import sessionmaker
-from moderation import MUBController, permission_index
+from moderation import MUBController
 from .locations_cli import manage_locations, upload_locations, delete_locations, mark_locations_updated
 from .locations_db import Place
 
-controller = MUBController("locations", path="/locations/", sessionmaker=sessionmaker)
 
+def setup(controller: MUBController = None) -> MUBController:
+    if controller is None:
+        controller = MUBController("locations", path="/locations/", sessionmaker=sessionmaker)
 
-@controller.route("/")
-class CitiesControlResource(Resource):
-    parser = RequestParser()
-    parser.add_argument("search", required=True)
-    parser.add_argument("strategy", type=int, required=False)
+    class CitiesControlResource(Resource):
+        parser = RequestParser()
+        parser.add_argument("search", required=True)
+        parser.add_argument("strategy", type=int, required=False)
 
-    @controller.doc_abort(400, "Empty search")
-    @permission_index.require_permission(controller, manage_locations, use_moderator=False)
-    @controller.argument_parser(parser)
-    @controller.marshal_list_with(Place.FullModel)
-    def get(self, session, search: str, strategy: int) -> list[Place]:
-        if len(search) == 0:
-            controller.abort(400, "Empty search")
-        t = time()
-        result = Place.get_all(session, search, strategy=strategy)
-        print(time() - t)
+        @controller.doc_abort(400, "Empty search")
+        @controller.require_permission(manage_locations, use_moderator=False)
+        @controller.argument_parser(parser)
+        @controller.marshal_list_with(Place.FullModel)
+        def get(self, session, search: str, strategy: int) -> list[Place]:
+            if len(search) == 0:
+                controller.abort(400, "Empty search")
+            t = time()
+            result = Place.get_all(session, search, strategy=strategy)
+            print(time() - t)
 
-        a = [(r.id, r.name) for r in result]
-        b = [(r.id, r.name) for r in Place.get_all(session, search, strategy=0 if strategy == 4 else 4)]
-        for i in range(len(a)):
-            if a[i] != b[i]:
-                print(i, a[i], b[i])
-        print()
+            a = [(r.id, r.name) for r in result]
+            b = [(r.id, r.name) for r in Place.get_all(session, search, strategy=0 if strategy == 4 else 4)]
+            for i in range(len(a)):
+                if a[i] != b[i]:
+                    print(i, a[i], b[i])
+            print()
 
-        return result
+            return result
 
-    parser = RequestParser()
-    parser.add_argument("csv", location="files", type=FileStorage, required=True)
+        parser = RequestParser()
+        parser.add_argument("csv", location="files", type=FileStorage, required=True)
 
-    @controller.doc_abort(400, "Invalid header")
-    @controller.doc_abort("400 ", "Invalid line")
-    @permission_index.require_permission(controller, manage_locations, use_moderator=False)
-    @controller.argument_parser(parser)
-    def post(self, session, csv: FileStorage):
-        try:
-            upload_locations(session, csv.stream)
-        except ValueError as e:
-            controller.abort(400, e.args[0])
+        @controller.doc_abort(400, "Invalid header")
+        @controller.doc_abort("400 ", "Invalid line")
+        @controller.require_permission(manage_locations, use_moderator=False)
+        @controller.argument_parser(parser)
+        def post(self, session, csv: FileStorage):
+            try:
+                upload_locations(session, csv.stream)
+            except ValueError as e:
+                controller.abort(400, e.args[0])
 
-    @permission_index.require_permission(controller, manage_locations, use_moderator=False)
-    def delete(self, session):
-        delete_locations(session)
+        @controller.require_permission(manage_locations, use_moderator=False)
+        def delete(self, session):
+            delete_locations(session)
 
+    class UpdatedMarkResource(Resource):
+        @controller.require_permission(manage_locations, use_session=False, use_moderator=False)
+        def post(self):
+            mark_locations_updated()
 
-@controller.route("/mark-updated/")
-class UpdatedMarkResource(Resource):
-    @permission_index.require_permission(controller, manage_locations, use_session=False, use_moderator=False)
-    def post(self):
-        mark_locations_updated()
+    controller.route("/")(CitiesControlResource)
+    controller.route("/mark-updated/")(UpdatedMarkResource)
+
+    return controller
